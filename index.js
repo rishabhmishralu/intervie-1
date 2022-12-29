f0971599a268ed4716c2fe0a6722e197e3168e2f
= <=>
2022-12-29 16:53:06 +0530

    add

---
+++ index.js
@@ -0,0 +1,27 @@
+const a=[2,3,4,4,"ram"]
+// const b=[1,5,6,8]
+// const c=[...a,...b]
+// console.log(c);
+
+// const b=[x,...rest]
+// console.log(a);
+
+
+// const student={
+//     name:"rishbah",
+//     lastname:"mishra",
+//     std:10
+// }
+
+
+// const student2={
+//     name:"aryan",
+//     lastname:"pal",
+//     std:11
+// }
+
+// function getname(n){
+//     console.log(this.name+" "+n);
+// }
+// getname.call(student,["delhi,lucknow"])
+
